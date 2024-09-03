6d3b5621668c97ede35dcd9b13d6a532cdabb635
Parag Chauhan <paragchauhan33@gmail.com>
2024-09-03 14:26:28 +0530

    Temporary commit to save work

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,5 +1,18 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./permit-to-work-software-firebase-adminsdk-37qoo-17fec4a36b.json');
+
+// Build the credentials object from environment variables
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLOUD_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_CLOUD_AUTH_URI,
+  token_uri: process.env.GOOGLE_CLOUD_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_CLOUD_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLOUD_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
